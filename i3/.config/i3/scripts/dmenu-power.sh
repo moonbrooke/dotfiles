02dfedf9fec26cff3b38168886a8e462c6e665f7
moonbrooke <smoeland@gmail.com>
2024-11-13 14:54:31 +0700

    add dmenu power menu script

---
+++ i3/.config/i3/scripts/dmenu-power.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+    
+case "$(echo -e "Shutdown\nRestart\nLogout\nSuspend\nLock" | dmenu \
+    -nb "${COLOR_BACKGROUND:-#1e1e2e}" \
+    -nf "${COLOR_DEFAULT:-#cdd6f4}" \
+    -sf "${COLOR_HIGHLIGHT:-#1e1e2e}" \
+    -sb "#89b4fa" \
+    -h 32 -i -p \
+    "Power:")" in
+        Shutdown) exec systemctl poweroff;;
+        Restart) exec systemctl reboot;;
+        Logout) kill -HUP $XDG_SESSION_PID;;
+        Suspend) exec systemctl suspend;;
+        Lock) exec systemctl --user start lock.target;;
+esac
