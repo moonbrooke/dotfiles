410c77c967318fb12d570c9a03e5acee0847e215
moonbrooke <smoeland@gmail.com>
2024-11-28 06:20:27 +0700

    nvim: add hid/show diagnostics keybind

--- nvim/.config/nvim/lua/core/keymaps.lua
+++ nvim/.config/nvim/lua/core/keymaps.lua
@@ -48,6 +48,8 @@ map("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic message
 map("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to prev diagnostic message" })
 map("n", "<leader>d", vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
 map("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
+map("n", "<leader>do", vim.diagnostic.show, { desc = "Show diagnostics" })
+map("n", "<leader>dh", vim.diagnostic.hide, { desc = "Hide diagnostics" })
 
 -- Duplicate
 map({ "n" }, "<C-S-A-Up>", "<CMD>LineDuplicate -1<CR>", { desc = "Duplicate line up" })
