847da770c53633dcdc04b3a7778435c65c95e9f4
moonbrooke <smoeland@gmail.com>
2025-11-17 17:30:52 +0700

    hypr-3: new hypr config once again

---
+++ hypr-3/.config/hypr/scripts/xdph.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+sleep 1
+killall -e xdg-desktop-portal-hyprland
+killall xdg-desktop-portal
+killall xdg-desktop-portal-gnome
+/usr/lib/xdg-desktop-portal-hyprland &
+sleep 2
+/usr/lib/xdg-desktop-portal &
