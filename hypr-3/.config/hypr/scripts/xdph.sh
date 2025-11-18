4974a658da731b9adac895a531f364de8c88f935
moonbrooke <smoeland@gmail.com>
2025-11-18 22:49:30 +0700

    hypr-3: edit env, xdph

--- hypr-3/.config/hypr/scripts/xdph.sh
+++ hypr-3/.config/hypr/scripts/xdph.sh
@@ -3,7 +3,7 @@
 sleep 1
 killall -e xdg-desktop-portal-hyprland
 killall xdg-desktop-portal
-killall xdg-desktop-portal-gnome
+killall xdg-desktop-portal-gtk
 /usr/lib/xdg-desktop-portal-hyprland &
 sleep 2
 /usr/lib/xdg-desktop-portal &
