5d5b5d9efe616e861bd3ea222214a17288f58891
moonbrooke <smoeland@gmail.com>
2025-11-22 12:51:06 +0700

    hypr-3: xdph

--- hypr-3/.config/hypr/scripts/xdph.sh
+++ hypr-3/.config/hypr/scripts/xdph.sh
@@ -4,6 +4,9 @@ sleep 1
 killall -e xdg-desktop-portal-hyprland
 killall xdg-desktop-portal
 killall xdg-desktop-portal-gtk
+killall xdg-desktop-portal-gnome
 /usr/lib/xdg-desktop-portal-hyprland &
 sleep 2
-/usr/lib/xdg-desktop-portal &
+/usr/lib/xdg-desktop-portal-gnome &
+sleep 2
+/usr/lib/xdg-desktop-portal-gtk &
