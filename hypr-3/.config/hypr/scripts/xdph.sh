02e3e26e75fa989123fad811cd6461997688928a
moonbrooke <smoeland@gmail.com>
2025-11-26 20:09:50 +0700

    hypr-3: edit autostart

--- hypr-3/.config/hypr/scripts/xdph.sh
+++ hypr-3/.config/hypr/scripts/xdph.sh
@@ -4,9 +4,6 @@ sleep 1
 killall -e xdg-desktop-portal-hyprland
 killall xdg-desktop-portal
 killall xdg-desktop-portal-gtk
-killall xdg-desktop-portal-gnome
 /usr/lib/xdg-desktop-portal-hyprland &
 sleep 2
-/usr/lib/xdg-desktop-portal-gnome &
-sleep 2
-/usr/lib/xdg-desktop-portal-gtk &
+/usr/lib/xdg-desktop-portal &
