d95629d625c9bf01d2d3c9651cbedd71e10d5345
moonbrooke <smoeland@gmail.com>
2026-03-05 11:44:44 +0700

    hypr-3: indent

--- hypr-3/.config/hypr/scripts/gamemode.sh
+++ hypr-3/.config/hypr/scripts/gamemode.sh
@@ -2,17 +2,17 @@
 HYPRGAMEMODE=$(hyprctl getoption animations:enabled | awk 'NR==1{print $2}')
 
 if [ "$HYPRGAMEMODE" = 1 ] ; then
-  hyprctl --batch "\
-      keyword animations:enabled 0;\
-      keyword decoration:shadow:enabled 0;\
-      keyword decoration:blur:enabled 0;\
-      keyword general:gaps_in 0;\
-      keyword general:gaps_out 0;\
-      keyword general:border_size 1;\
-      keyword decoration:rounding 0"
+    hyprctl --batch "\
+        keyword animations:enabled 0;\
+        keyword decoration:shadow:enabled 0;\
+        keyword decoration:blur:enabled 0;\
+        keyword general:gaps_in 0;\
+        keyword general:gaps_out 0;\
+        keyword general:border_size 1;\
+        keyword decoration:rounding 0"
 
-  pkill waybar
-  exit
+    pkill waybar
+    exit
 fi
 
 hyprctl reload
