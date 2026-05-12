e496a16a3b0aaa238904f5d0ddd9ba5e7712b188
moonbrooke <smoeland@gmail.com>
2026-05-13 03:19:52 +0700

    hypr-lua: edit _rule

--- hypr-lua/.config/hypr/_rule.lua
+++ hypr-lua/.config/hypr/_rule.lua
@@ -1,46 +1,29 @@
 hl.window_rule({ match = { title = "Bluetooth" }, float = true })
 hl.window_rule({ match = { title = "floating_wifi" }, size = {800, 800}, float = true })
-hl.window_rule({ match = { class = "org.prismlauncher.PrismLauncher" }, float = true })
-hl.window_rule({ match = { class = "UltimMC" }, float = true })
-hl.window_rule({ match = { class = "minecraft-launcher" }, float = true })
-hl.window_rule({ match = { title = "^(.*Minecraft.*)$" }, float = true })
-hl.window_rule({ match = { class = "steam" }, float = true })
-hl.window_rule({ match = { class = "^(steam_app_.*)$" }, float = true })
 hl.window_rule({ match = { class = "org.gnome.nautilus" }, float = true })
 hl.window_rule({ match = { class = "org.gnome.Loupe" }, float = true })
+hl.window_rule({ match = { class = "org.gnome.Calculator" }, float = true })
 hl.window_rule({ match = { class = "com.github.rafostar.Clapper" }, float = true })
+hl.window_rule({ match = { class = "com.github.wwmm.easyeffects" }, workspace = 3, float = true })
 hl.window_rule({ match = { class = "com.obsproject.Studio" }, workspace = 4, float = false })
-
+hl.window_rule({ match = { class = "org.telegram.desktop" }, float = true })
+hl.window_rule({ match = { class = "org.pulseaudio.pavucontrol" }, float = true })
+hl.window_rule({ match = { class = "net.tagaini.tagainijisho" }, float = true })
+hl.window_rule({ match = { title = "Picture-in-Picture" }, float = true })
+hl.window_rule({ match = { class = "discord" }, float = true })
+hl.window_rule({ match = { class = "vlc" }, opacity = "1.0", float = true })
+hl.window_rule({ match = { class = "imv" }, float = true })
+hl.window_rule({ match = { class = "LM-Studio" }, workspace = 2, float = true })
+hl.window_rule({ match = { class = "Mailspring" }, workspace = 2, float = false })
 hl.window_rule({ match = { title = "^(.*YouTube.*)$" }, opacity = "1.0" })
+hl.window_rule({ match = { title = "^(.*Twitch.*)$" }, opacity = "1.0" })
 
--- windowrule = {
---     -- Floats
---     "float on, match:class (org.telegram.desktop)",
---     "float on, match:class (discord)",
---     "float on, match:class (net.tagaini.tagainijisho)",
---     "float on, match:class (vlc)",
---     "float on, match:title (Picture-in-Picture)",
---     "float on, match:class (org.pulseaudio.pavucontrol)",
---     "float on, match:class (lutris)",
---     "float on, match:class (org.gnome.Calculator)",
---     "float on, match:class (imv)",
---
---     -- Workspaces & Floats
---     "float on, workspace 3, match:class (com.github.wwmm.easyeffects)",
---     "workspace 2, match:class (Mailspring)",
---     "float on, workspace 2, match:class (LM-Studio)",
---
---     -- Opacity
---     "opacity 1.0 override 1.0 override, match:title ^(.*Twitch.*)$",
---     "opacity 1.0 override 1.0 override, match:title (Picture-in-Picture)",
---     "opacity 1.0 override 1.0 override, match:class (obs)",
---     "opacity 1.0 override 1.0 override, match:class (org.gnome.Loupe)",
---     "opacity 1.0 override 1.0 override, match:class (com.github.rafostar.Clapper)",
---     "opacity 1.0 override 1.0 override, match:class (vlc)",
---     "opacity 1.0 override 1.0 override, match:title ^(.*Minecraft.*)$",
---     "opacity 1.0 override 1.0 override, match:class (com.bitwig.BitwigStudi)",
---     "opacity 1.0 override 1.0 override, match:class ^(steam_app_.*)$"
--- },
+hl.window_rule({ match = { class = "org.prismlauncher.PrismLauncher" }, float = true })
+hl.window_rule({ match = { class = "minecraft-launcher" }, float = true })
+hl.window_rule({ match = { title = "^(.*Minecraft.*)$" }, opacity = "1.0", float = true })
+hl.window_rule({ match = { class = "steam" }, float = true })
+hl.window_rule({ match = { class = "^(steam_app_.*)$" }, opacity = "1.0", float = true })
+hl.window_rule({ match = { class = "lutris" }, float = true })
 
 hl.workspace_rule({ workspace = "1", persistent = true, monitor = "eDP-1", default = true, })
 hl.workspace_rule({ workspace = "2", persistent = true, monitor = "eDP-1", default = true, })
