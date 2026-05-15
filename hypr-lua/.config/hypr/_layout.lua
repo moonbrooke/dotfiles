40c9d9329f7fe90b772e0d31df99e8adc52e0fda
moonbrooke <smoeland@gmail.com>
2026-05-15 17:10:02 +0700

    hypr-lua, waybar-2: layout indicator

--- hypr-lua/.config/hypr/_layout.lua
+++ hypr-lua/.config/hypr/_layout.lua
@@ -1,21 +1,25 @@
 local layouts = { "dwindle", "master", "scrolling", "monocle" }
-local img = "~/.config/hypr/img/window.png"
 
 return function()
     local current_layout = hl.get_config("general.layout")
     local next_index = 1
+
     for i, layout in ipairs(layouts) do
         if layout == current_layout then
             next_index = (i % #layouts) + 1
             break
         end
     end
+
     local next_layout = layouts[next_index]
+
     hl.config({ general = { layout = next_layout } })
 
     local cmd = string.format(
         "notify-send 'Layout Changed' 'Switched to: <b>%s</b>' -t 2500 -i dialog-information &",
         next_layout
     )
+
     os.execute(cmd)
+    os.execute("pkill -RTMIN+8 waybar &")
 end
