32b63ae8c786f46eee753bb80e7a92477ce05536
moonbrooke <smoeland@gmail.com>
2026-05-17 07:20:30 +0700

    scripts: edit rofi-notes.sh

--- scripts/scripts/rofi-notes.sh
+++ scripts/scripts/rofi-notes.sh
@@ -3,10 +3,10 @@
 folder=$HOME/notes/
 
 newnote () { \
-  dir="$(find "$folder" -maxdepth 1 -type d | rofi -dmenu -i -l 11 -p 'Choose directory: ')" || exit 0
+  dir="$(find "$folder" -maxdepth 1 -type d ! -name ".git" | rofi -dmenu -i -l 11 -p 'Choose directory: ' -theme-str 'window {width: 35%; border: 3px; border-color: #24283b;}')" || exit 0
   : "${dir:=$folder}"
   
-  name="$(echo "" | rofi -dmenu -theme-str 'listview {lines: 0;}' -p "Enter a name: " <&-)" || exit 0
+  name="$(echo "" | rofi -dmenu -theme-str 'window {width: 35%; border: 3px; border-color: #24283b;} listview {lines: 0;}' -p "Enter a name: " <&-)" || exit 0
   : "${name:=$(date +%F_%H-%M-%S)}"
   
   setsid -f foot -e nvim "${dir%/}/$name.md" >/dev/null 2>&1
@@ -14,11 +14,11 @@ newnote () { \
 
 selected () { \
   choice=$(
-    echo -e "New\n$(find "$folder" -type f -printf '%T@ %P\n' | sort -nr | cut -d' ' -f2-)" | rofi -dmenu -i -l 11 -p "Select note or write new one: "
+    echo -e "New\n$(find "$folder" -name ".git" -prune -o -type f \( -name "*.md" -o -name "*.txt" \) -printf '%T@ %P\n' | sort -nr | cut -d' ' -f2-)" | rofi -dmenu -i -l 11 -p "Select note or write new one: " -theme-str 'window {width: 35%; border: 3px; border-color: #24283b;}'
   )
   case $choice in
     New) newnote ;;
-    *.md) setsid -f foot -e nvim "$folder$choice" >/dev/null 2>&1 ;;
+    *.md|*.txt) setsid -f foot -e nvim "$folder$choice" >/dev/null 2>&1 ;;
     *) exit ;;
   esac
 }
