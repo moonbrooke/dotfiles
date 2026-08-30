3c6194784512fefd0fa14c0a7dad5573a25648b1
moonbrooke <smoeland@gmail.com>
2026-08-31 05:16:29 +0700

    edit

--- nvidia.sh
+++ nvidia.sh
@@ -1,2 +1,2 @@
 sudo pacman -Syu --noconfirm
-sudo pacman -S nvidia-open-dkms nvidia-utils egl-wayland linux-headers --noconfirm --needed
+sudo pacman -S nvidia-open-dkms nvidia-utils egl-wayland linux-zen-headers --noconfirm --needed
