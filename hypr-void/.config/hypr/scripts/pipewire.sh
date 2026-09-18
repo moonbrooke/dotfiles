5e0613af51f0c9d25817a6f0b6ef3038568a0746
moonbrooke <smoeland@gmail.com>
2026-09-18 19:43:41 +0700

    hypr-void: autostart, xdph, env stuff

---
+++ hypr-void/.config/hypr/scripts/pipewire.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+check_and_start() {
+    name=$1
+    bin=$2
+
+    if ! pgrep -x "$name" >/dev/null 2>&1; then
+        echo "Starting $name..."
+        sleep 1
+        "$bin" &
+    else
+        echo "$name is already running"
+    fi
+}
+
+check_and_start "pipewire" "/usr/bin/pipewire"
